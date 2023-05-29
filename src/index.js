c0987341e853b34d7920cf5d0b5401b10cd216af
DreamyTZK <i@tzki.cn>
2023-05-29 22:55:22 +0800

    feat: 更新代码

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path')
+
 /**
  * 获取站点列表
- * @returns Promise<any[]>
+ * @returns {Promise<{link:string,name:string,id:string}[]>}
  */
-function getSiteList () {
-  return new Promise(resolve => {
-    resolve([
-      {
-        site: 'https://www.imlete.cn',
-        name: 'lete'
-      }
-    ])
-  })
+async function getSiteList () {
+  const result = await axios.get('https://api.antmoe.com/api/open/website/list?day=7')
+  return result.data.data
 }
 
 
 async function getShot (url, name) {
-
   const response = await axios.get(`https://image.thum.io/get/width/1024/crop/768/${url}`, {
     responseType: 'arraybuffer'
   })
 
   if (response.status === 200) {
     fs.writeFile(path.resolve(__dirname,'..','dist',name+'.png'), response.data, function (err) {
-      if (err) return console.log(`${name}保存失败：${err}`);
+      if (err) return console.log(`${name}保存失败：${err.message}`);
       console.log(`${name}保存成功：${url}`);
     });
   }
@@ -35,7 +29,7 @@ async function getShot (url, name) {
 async function main () {
    const siteList = await getSiteList()
    siteList.forEach(item => {
-     getShot(item.site, item.name)
+     getShot(item.link, item.name)
    })
 }
 
