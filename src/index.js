173eeb48282d52a51946ad7377ebc7878308e5e2
DreamyTZK <i@tzki.cn>
2023-05-29 17:48:26 +0800

    init: 初始化仓库

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path')
+/**
+ * 获取站点列表
+ * @returns Promise<any[]>
+ */
+function getSiteList () {
+  return new Promise(resolve => {
+    resolve([
+      {
+        site: 'https://www.baidu.com',
+        name: 'baidu'
+      }
+    ])
+  })
+}
+
+
+async function getShot (url, name) {
+
+  const response = await axios.get(`https://image.thum.io/get/width/1024/crop/768/${url}`, {
+    responseType: 'arraybuffer'
+  })
+
+  if (response.status === 200) {
+    fs.writeFile(path.resolve(__dirname,'..','dist',name+'.png'), response.data, function (err) {
+      if (err) return console.log(`${name}保存失败：${err}`);
+      console.log(`${name}保存成功：${url}`);
+    });
+  }
+
+}
+
+async function main () {
+   const siteList = await getSiteList()
+   siteList.forEach(item => {
+     getShot(item.site, item.name)
+   })
+}
+
+main()
