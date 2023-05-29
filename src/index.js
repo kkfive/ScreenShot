b8f4fba1ac0ec2a930298cf0c9b6675191e5d297
DreamyTZK <i@tzki.cn>
2023-05-29 23:06:10 +0800

    chore: 调整脚本

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path')
-
+const { LSKY_7BU, KEY, SECRET } = process.env
 /**
  * 获取站点列表
  * @returns {Promise<{link:string,name:string,id:string}[]>}
@@ -13,24 +13,37 @@ async function getSiteList () {
 
 
 async function getShot (url, name) {
-  const response = await axios.get(`https://image.thum.io/get/width/1024/crop/768/${url}`, {
-    responseType: 'arraybuffer'
-  })
+  return new Promise(async (resolve) => {
+    const response = await axios.get(`https://image.thum.io/get/width/1024/crop/768/${url}`, {
+      responseType: 'arraybuffer'
+    })
 
-  if (response.status === 200) {
-    fs.writeFile(path.resolve(__dirname,'..','dist',name+'.png'), response.data, function (err) {
-      if (err) return console.log(`${name}保存失败：${err.message}`);
-      console.log(`${name}保存成功：${url}`);
-    });
-  }
+    if (response.status === 200) {
+      fs.writeFile(path.resolve(__dirname, '..', 'dist', name + '.png'), response.data, function (err) {
+        resolve()
+        if (err) return console.log(`${name}保存失败：${err.message}`);
+        console.log(`${name}保存成功：${url}`);
+      });
+    }
+  })
 
 }
 
+async function uoload7Bu () {
+ const result = await axios.post('https://7bu.top/api/v1/upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Accept: 'application/json',
+      Authorization: LSKY_7BU,
+    },
+  })
+  return result.data;
+}
+
+
 async function main () {
-   const siteList = await getSiteList()
-   siteList.forEach(item => {
-     getShot(item.link, item.name)
-   })
+  const siteList = await getSiteList()
+  await Promise.all(siteList.map(item => getShot(item.link, item.name)))
 }
 
 main()
