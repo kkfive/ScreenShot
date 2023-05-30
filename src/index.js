a64a857249a1422ce765183b2fa6a93f44df10af
DreamyTZK <i@tzki.cn>
2023-05-30 22:46:05 +0800

    feat: 更新脚本

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path')
+const FormData = require('form-data');
 const { LSKY_7BU, KEY, SECRET } = process.env
-// const webstackScreenshot = require('webstack-screenshot');
 /**
  * 获取站点列表
  * @returns {Promise<{link:string,name:string,id:string}[]>}
  */
 async function getSiteList () {
-  const result = await axios.get('https://api.antmoe.com/api/open/website/list?day=7')
+  const result = await axios.get('https://api.antmoe.com/api/open/website/list?day=0')
   return result.data.data
 }
 
@@ -19,8 +19,6 @@ async function getShot (url, name) {
       responseType: 'arraybuffer'
     })
     const buffer = response.data
-    // const buffer = await webstackScreenshot({url})
-
     if (buffer) {
       fs.writeFile(path.resolve(__dirname, '..', 'dist', name + '.png'), buffer, function (err) {
         resolve()
@@ -32,8 +30,14 @@ async function getShot (url, name) {
 
 }
 
-async function uoload7Bu () {
- const result = await axios.post('https://7bu.top/api/v1/upload', formData, {
+async function uoload7Bu (name) {
+
+  const data = new FormData();
+  data.append('file', fs.createReadStream(path.resolve(__dirname, '..', 'dist', name + '.png')));
+  data.append('album_id', '398');
+  data.append('permission', 1);
+
+  const result = await axios.post('https://7bu.top/api/v1/upload', data, {
     headers: {
       'Content-Type': 'multipart/form-data',
       Accept: 'application/json',
@@ -47,6 +51,20 @@ async function uoload7Bu () {
 async function main () {
   const siteList = await getSiteList()
   await Promise.all(siteList.map(item => getShot(item.link, item.name)))
+  for (const site of siteList) {
+    const result = await uoload7Bu(site.name)
+    const shot = result.data.links.url
+    const shotKey = result.data.key
+    await axios.post('https://api.antmoe.com/api/open/manager/website/update', {
+      id: site.id,
+      shot,
+      shotKey,
+      key: KEY,
+      secret: SECRET,
+    })
+    console.log(`${site.name}更新成功：${shot}`)
+
+  }
 }
 
 main()
