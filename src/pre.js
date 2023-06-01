2d99623eb9956cb4d99697dde1449f6b791915b7
DreamyTZK <i@tzki.cn>
2023-06-01 09:54:06 +0800

    feat: 添加预请求任务

--- src/pre.js
+++ src/pre.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path')
-const FormData = require('form-data');
-const { LSKY_7BU, KEY, SECRET } = process.env
 /**
  * 获取站点列表
  * @returns {Promise<{link:string,name:string,id:string}[]>}
@@ -30,40 +28,10 @@ async function getShot (url, name) {
 
 }
 
-async function uoload7Bu (name) {
-
-  const data = new FormData();
-  data.append('file', fs.createReadStream(path.resolve(__dirname, '..', 'dist', name + '.png')));
-  data.append('album_id', '398');
-  data.append('permission', 1);
-
-  const result = await axios.post('https://7bu.top/api/v1/upload', data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-      Accept: 'application/json',
-      Authorization: LSKY_7BU,
-    },
-  })
-  return result.data;
-}
-
 
 async function main () {
   const siteList = await getSiteList()
   await Promise.all(siteList.map(item => getShot(item.link, item.name)))
-  for (const site of siteList) {
-    const result = await uoload7Bu(site.name)
-    const shot = result.data.links.url
-    const shotKey = result.data.key
-    await axios.post('https://api.antmoe.com/api/open/manager/website/update', {
-      id: site.id,
-      shot,
-      shotKey,
-      key: KEY,
-      secret: SECRET,
-    })
-    console.log(`【${site.name}】更新成功：${shot}`)
-  }
 }
 
 main()
